9b9a8109ce7d61b1eb8caaf275df2045c7e2e323
Amelie Gonzalez <ameliegonzalez95@gmail.com>
2025-08-21 01:45:49 +0200

    Amélioration du css

--- views/header.php
+++ views/header.php
@@ -18,7 +18,7 @@
     <!-- Importation de Bootstrap 5 via un CDN pour le style et la mise en page -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <!-- Lien vers la feuille de style CSS personnalisée -->
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/main.css">
     <!-- Titre affiché dans l’onglet du navigateur -->
     <title>Ecoride</title>
 </head>
