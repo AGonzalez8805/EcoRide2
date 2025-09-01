14bd78cd45a6fc293259f6f89857c45360ac1b39
Amelie Gonzalez <ameliegonzalez95@gmail.com>
2025-09-02 00:29:49 +0200

    ...

--- views/covoiturage/ajouter.php
+++
@@ -1,8 +0,0 @@
-<?php require_once APP_ROOT . '/views/header.php'; ?>
-
-<h1>Ajouter un covoiturage</h1>
-<form method="POST" action="index.php?controller=covoiturage&action=ajouter">
-    <!-- Champs du formulaire -->
-</form>
-
-<?php require_once APP_ROOT . '/views/footer.php'; ?>
